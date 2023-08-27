5212d99487af4929e2f851bbd517c8e3d387c621
Yelyzaveta <molochiza@gmail.com>
2023-08-27 19:37:42 +0300

    add initial HTML

---
+++ main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.our-works__swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  speed: 900,
+});
