3faf513966ae3194a952756314bf88cef6f6e3ac
Yelyzaveta <molochiza@gmail.com>
2023-09-05 22:21:35 +0300

    initial html

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+'use strict'
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 const swiper = new Swiper('.our-works__swiper', {
   direction: 'horizontal',
   loop: true,
